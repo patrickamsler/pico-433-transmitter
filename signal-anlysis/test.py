34b99604b93f2816b29978f7c3c45b20e24c3c55
Patrick Amsler <patrick.amsler@abraxas.ch>
2023-05-07 17:46:03 +0200

    plot frames

--- signal-anlysis/test.py
+++ signal-anlysis/test.py
@@ -10,12 +10,15 @@ def main():
 
     #Normalize the amplitude values
     amplitude_norm = np.where(amplitude >= 1.2, 1, 0)
-    
-    countedges(amplitude_norm)
-    
-    # display the plot
-    plot(time, amplitude_norm)
+    # plot full signal
+    #plot(time, amplitude_norm)
 
+    edges = find_edges(amplitude_norm)
+
+    frames = find_frames(amplitude_norm, time, edges)
+    for f in frames:
+        plot(f['time'], f['amplitude'])
+    
 def plot(time, amplitude):
     # Create a line plot of the data
     plt.plot(time, amplitude)
@@ -27,23 +30,32 @@ def plot(time, amplitude):
 
 def loaddata():
     # Load the data from the CSV file
-    data = pd.read_csv('signal-sample/test2/samples-a-on-2.csv', skiprows=[0], header=None, names=['time', 'amplitude'])
+    data = pd.read_csv('signal-sample/test2/samples-a-on-1.csv', skiprows=[0], header=None, names=['time', 'amplitude'])
     # Extract the time and amplitude values
     time = data['time'].values
     amplitude = data['amplitude'].values
     return time, amplitude
 
-def countedges(amplitude):
+def find_edges(amplitude):
     # find the edges
     edges = np.where(np.diff(amplitude) != 0)
-    print('Edges: {}'.format(edges))
-    
     # Count the number of edges
-    numofedges = len(edges[0])
-    print('Number of edges: {}'.format(numofedges))
-    
-    frames = numofedges / 2 / 25 # one edge for positive and one for negative, 25 positive edges per frame
-    print('Number of frames: {}'.format(frames))
+    num_of_edges = len(edges[0])
+    print('Number of edges: {}'.format(num_of_edges))
+    return edges[0]
+
+def find_frames(amplitude, time, edges):
+    # one edge for positive and one for negative, 25 positive edges per frame
+    num_of_frames = len(edges) / 2 / 25
+    print('Number of frames: {}'.format(num_of_frames))
+    # split the amplitude into frames
+    edges_arr = np.split(edges, num_of_frames)
+    frames = []
+    for e in edges_arr:
+        amp_sub = amplitude[e[0]:e[-1]+2]
+        time_sub = time[e[0]:e[-1]+2]
+        frames.append({'time': time_sub, 'amplitude': amp_sub})
+    return frames
 
 # Call the main function
 main()

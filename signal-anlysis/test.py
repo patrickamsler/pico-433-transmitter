dba032976f9abbce4a90e0d637673adbfe5bc8f5
Patrick Amsler <patrick.amsler@abraxas.ch>
2023-05-09 10:45:24 +0200

    some refactoring

--- signal-anlysis/test.py
+++ signal-anlysis/test.py
@@ -5,24 +5,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+
 def main():
     time, amplitude = loaddata()
 
-    #Normalize the amplitude values
+    # Normalize the amplitude values
     amplitude_norm = np.where(amplitude >= 1.2, 1, 0)
     # plot full signal
-    #plot(time, amplitude_norm)
+    # plot(time, amplitude_norm)
 
     edges = find_edges(amplitude_norm)
 
     frames = find_frames(amplitude_norm, time, edges)
-    for i,f in enumerate(frames):
-        bits = frame_to_bits(f)
-        dec = int(bits, 2)
-        hex = format(dec, '02x')
-        print('Frame {}: {} = {} = {}'.format(i, bits, dec, hex))
+    for i, f in enumerate(frames):
+        print('Frame {}: {} = {}'.format(i, f['bits'], f['hex']))
         plot(f['time'], f['amplitude'])
-    
+
+
 def plot(time, amplitude):
     # Create a line plot of the data
     plt.plot(time, amplitude)
@@ -32,13 +31,16 @@ def plot(time, amplitude):
     plt.title('Amplitude vs Time')
     plt.show()
 
+
 def loaddata():
     # Load the data from the CSV file
-    data = pd.read_csv('signal-sample/test2/samples-a-on-1.csv', skiprows=[0], header=None, names=['time', 'amplitude'])
+    data = pd.read_csv('signal-sample/test2/samples-a-on-1.csv',
+                       skiprows=[0], header=None, names=['time', 'amplitude'])
     # Extract the time and amplitude values
     time = data['time'].values
     amplitude = data['amplitude'].values
     return time, amplitude
+
 
 def find_edges(amplitude):
     # find the edges
@@ -48,6 +50,7 @@ def find_edges(amplitude):
     print('Number of edges: {}'.format(num_of_edges))
     return edges[0]
 
+
 def find_frames(amplitude, time, edges):
     # one edge for positive and one for negative, 25 positive edges per frame
     num_of_frames = len(edges) / 2 / 25
@@ -55,34 +58,42 @@ def find_frames(amplitude, time, edges):
     # split the amplitude into frames
     edges_arr = np.split(edges, num_of_frames)
     frames = []
-    for i,e in enumerate(edges_arr):
+    for i, e in enumerate(edges_arr):
         first = e[0]
         if i+1 >= len(edges_arr):
             last = len(amplitude)
         else:
             last = edges_arr[i+1][0]
-        
+
         amp_sub = amplitude[first:last]
         time_sub = time[first:last]
         e_norm = e - e[0]
-        frames.append({'time': time_sub, 'amplitude': amp_sub, 'edges': e_norm})
+
+        # build frame
+        bits = calc_bits(e_norm, amp_sub)
+        hex = format(int(bits, 2), 'x')
+        frame = {'time': time_sub, 'amplitude': amp_sub,
+                 'edges': e_norm, 'bits': bits, 'hex': hex}
+        frames.append(frame)
     return frames
 
-def frame_to_bits(frame):
+
+def calc_bits(edges, amplitude):
     bits = ''
-    for i in range(2, len(frame['edges']), 2):
-        first_edge = frame['edges'][i]
-        if i+2 >= len(frame['edges']):
-            second_edge = len(frame['amplitude'])
+    for i in range(2, len(edges), 2):
+        first_edge = edges[i]
+        if i+2 >= len(edges):
+            second_edge = len(amplitude)
         else:
-            second_edge = frame['edges'][i+2]
-        amp_sub = frame['amplitude'][first_edge:second_edge]
+            second_edge = edges[i+2]
+        amp_sub = amplitude[first_edge:second_edge]
         # check if array contains more zeros or ones
         if np.count_nonzero(amp_sub) > len(amp_sub)/2:
             bits += '1'
         else:
             bits += '0'
     return bits
+
 
 # Call the main function
 main()
